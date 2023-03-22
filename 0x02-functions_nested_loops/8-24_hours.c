76851399626c7bfaaa7168db01be5c2111a2dc2c
Ven369mr <ourmery@gmail.com>
2023-03-21 18:26:07 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,21 +9,21 @@ void jack_bauer(void)
 {
 int a, b, c, d;
 
-for (a = 48; a <= 50; a++)
+for (a = 0; a <= 2; a++)
 {
-for (b = 48; b <= 57; b++)
+for (b = 0; b <= 9; b++)
 {
-for (c = 48; c <= 53; c++)
+for (c = 0; c <= 5; c++)
 {
-for (d = 48; d <= 57; d++)
+for (d = 0; d <= 9; d++)
 {
-if (a >= 50 && b >= 52)
+if (a >= 2 && b >= 4)
 break;
-_putchar(a);
-_putchar(b);
+_putchar(a + 48);
+_putchar(b + 48);
 _putchar(58);
-_putchar(c);
-_putchar(d);
+_putchar(c + 48);
+_putchar(d + 48);
 _putchar('\n')
 }
 }
