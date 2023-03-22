57d641f135953603e1dcebacbd6ed079a52dfe5c
Ven369mr <ourmery@gmail.com>
2023-03-21 17:26:57 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void) 
+void print_alphabet(void)
 {
-	int i;
+int i;
 
-	for (i = 97; i <= 122; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
