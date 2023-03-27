831fdd2149f7a4c3546183e8495fc27e7161be49
Ven369mr <ourmery@gmail.com>
2023-03-27 11:11:20 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,6 @@
-include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include < stdio.h >
+#include < stdlib.h >
+#include < time.h >
 
 /**
  * main - Generates random valid passwords for the
@@ -14,22 +14,18 @@ int main(void)
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
-
 	while (sum < 2772)
 	{
 		password[index] = 33 + rand() % 94;
 		sum += password[index++];
 	}
-
 	password[index] = '\0';
-
 	if (sum != 2772)
 	{
 		diff_half1 = (sum - 2772) / 2;
 		diff_half2 = (sum - 2772) / 2;
 		if ((sum - 2772) % 2 != 0)
 			diff_half1++;
-
 		for (index = 0; password[index]; index++)
 		{
 			if (password[index] >= (33 + diff_half1))
@@ -49,6 +45,5 @@ int main(void)
 	}
 
 	printf("%s", password);
-
 	return (0);
 }
